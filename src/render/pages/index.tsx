56814e2d342fca508d543bc0920cf6aeb1c2da82
草鞋没号 <308487730@qq.com>
2020-06-01 09:46:06 +0800

    release@1.0.0

--- src/render/pages/index.tsx
+++ src/render/pages/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Alert } from 'antd';
 import styles from './index.less';
 import yay from '@/assets/yay.jpg';
 
 export default () => {
   return (
-    <div className={styles.page} style={{ backgroundImage: `url(${yay})` }} />
+    <div className={styles.page} style={{ backgroundImage: `url(${yay})` }}>
+      <Alert message="作者QQ: 308487730" />
+    </div>
   );
 };
