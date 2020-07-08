02d65f5f4277fa1ee9a71e7bfc22ada896ac6e87
草鞋没号 <308487730@qq.com>
2020-07-08 11:44:47 +0800

    somthing docs。

---
+++ docs/tray.js
@@ -0,0 +1,33 @@
+/**
+ * 最小化到托盘  windows系统可用
+ */
+
+const { app, BrowserWindow, Tray, Menu } = require('electron');
+const path = require('path');
+
+let tray;
+global.tray = tray;
+
+export default function createTray() {
+  const mainWindow = BrowserWindow.fromId(global.mainId);
+  const iconName = process.platform === 'win32' ? 'icon.ico' : 'icon.png';
+  tray = new Tray(path.join(global.__dirname, iconName));
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: '显示主界面',
+      click: () => {
+        mainWindow.show();
+        mainWindow.setSkipTaskbar(false);
+      },
+    },
+    {
+      label: '退出',
+      click: () => {
+        mainWindow.destroy();
+        app.quit();
+      },
+    },
+  ]);
+  tray.setToolTip('electron-react');
+  tray.setContextMenu(contextMenu);
+}
